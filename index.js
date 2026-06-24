48e0620bf08f6967f5c1174da78671774acc1d53
Anshumaan Mishra <anshumaan.1611@gmail.com>
2026-06-24 21:27:47 +0530

    chore: update root endpoint message to include version identifier

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.json({ msg: "Hello Github actions" })
+    res.json({ msg: "Hello Github actions v1" })
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
